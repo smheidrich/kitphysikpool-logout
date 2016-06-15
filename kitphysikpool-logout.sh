b9f8f4450776f555413bf66efe600f998e0ae0f3
smheidrich <smheidrich@weltenfunktion.de>
2016-06-15 18:22:12 +0200

    Added preliminary fancy progress indicator.

--- kitphysikpool-logout.sh
+++ kitphysikpool-logout.sh
@@ -5,16 +5,18 @@ NUM_HOSTS=30
 i=1
 
 while (( i < 30 )); do
+  echo "pruefe: $host"
   host="$(printf "$HOST_TEMPLATE" "$i")"
   echo -n users \
   | ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
   "$USER"@"$host" 2>/dev/null \
   | grep "$USER" >&/dev/null \
   && [ "$host" != "$(hostname)" ] \
-  && echo -n "eingeloggt auf: $host" \
+  && printf '\x1B[A' && echo -n "eingeloggt auf: $host" \
   && echo "killall startkde" \
   | ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
   "$USER"@"$host" 2>/dev/null \
-  && echo " - beendet"
+  && echo " - beendet" \
+  || printf '\x1B[A'
   ((i++))
 done
